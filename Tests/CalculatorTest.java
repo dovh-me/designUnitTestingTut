56f2af45b8ca372965b22c78b58dbd51016cbace
h_osura <h.osura@directfn.com>
2021-03-20 18:24:30 +0530

    Initial Commit

---
+++ Tests/CalculatorTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+        int firstNumber = 10;
+
+        int secondNumber = 20;
+
+        int expectedOutput = 30;
+
+        Calculator calculatorTest = new Calculator();
+        int actualOutput = calculatorTest.add(firstNumber,secondNumber);
+        assertEquals(expectedOutput,actualOutput);
+    }
+
+    @Test
+    void multiply() {
+        int firstNumber = 10;
+
+        int secondNumber = 20;
+
+        int expectedOutput = 200;
+
+        Calculator calculatorTest = new Calculator();
+        int actualOutput = calculatorTest.multiply(firstNumber,secondNumber);
+        assertEquals(expectedOutput,actualOutput);
+    }
+
+    @Test
+    void division() {
+        int firstNumber = 20;
+
+        int secondNumber = 0;
+
+        int expectedOutput = 200;
+
+        Calculator calculatorTest = new Calculator();
+
+        assertThrows(ArithmeticException.class, ()->{
+            calculatorTest.division(firstNumber, secondNumber);
+        });
+    }
+}
---
+++ src/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    public int add(int firstNumber, int secondNumber){
+        return firstNumber + secondNumber;
+    }
+    public int multiply(int firstNumber, int secondNumber){
+        return firstNumber * secondNumber;
+    }
+    public int division(int firstNumber, int secondNumber){
+        return firstNumber / secondNumber;
+    }
+}
